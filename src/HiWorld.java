783b65bc6abbcbfe59eb4d78bd36ade46f6b80d2
盛凯 <411448753@qq.com>
2020-05-11 14:26:35 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("Hi world");
-        System.out.println("test");
+        System.out.println("test 提交更新");
     }
 }
